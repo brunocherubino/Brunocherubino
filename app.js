e9afb23404e09c9c019cb8cecebb1da2f30be26a
brunocherubino <80709302+brunocherubino@users.noreply.github.com>
2021-12-27 13:54:21 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const hamburguer = document.querySelector('.hamburguer');
+const menu = document.querySelector('.navbar');
+
+hamburguer.addEventListener('click', () => {
+    menu.classList.toggle("spread")
+})
+
+window.addEventListener('click', e => {
+    if (menu.classList.contains('spread') && e.target !=
+        menu && e.target != hamburguer) {
+
+        menu.classList.toggle("spread")
+    }
+})
